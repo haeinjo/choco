457d9ad85c9deaada77f278ca5f912ddbfc13d09
haeinjo <godls036@gmail.com>
2020-01-14 22:27:19 +0900

    modify evaluation admin and have to add score part in modeds, admin of evaluation.

--- pt1/evaluation/admin.py
+++ pt1/evaluation/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Tag, SongOwnTag, SongCoveredTag
+from .models import Tag, SongOwnTag, SongCoveredTag, ScoreOwn, ScoreCovered
 
 # Register your models here.
 
@@ -20,3 +20,25 @@ class SongCoveredTagAdmin(admin.ModelAdmin):
     list_display = ('song', 'tag')
 
 admin.site.register(SongCoveredTag, SongCoveredTagAdmin)
+
+
+class ScoreOwnAdmin(admin.ModelAdmin):
+    model = ScoreOwn
+    list_display = ('get_title', 'score')
+
+    def get_title(self, obj):
+        return obj.song.title
+    get_title.short_description = '자작곡'
+
+admin.site.register(ScoreOwn, ScoreOwnAdmin)
+
+
+class ScoreCoveredAdmin(admin.ModelAdmin):
+    model = ScoreCovered
+    list_display = ('get_title', 'score')
+
+    def get_title(self, obj):
+        return obj.song.title
+    get_title.short_description = '커버곡'
+
+admin.site.register(ScoreCovered, ScoreOwnAdmin)
