c210fbb89f1059a8b12cb30c245c28d28c535666
haeinjo <godls036@gmail.com>
2020-01-15 18:56:31 +0900

    add songOwnTagDetail, songCoveredTagDetail

--- pt1/evaluation/urls.py
+++ pt1/evaluation/urls.py
@@ -1,17 +1,19 @@
 from django.urls import path
-from .views import TagList, TagDetail, SongOwnTagList, SongOwnTagDetail, SongCoveredTagList, SongCoveredTagDetail, ScoreOwnList, ScoreOwnDetail, ScoreCoveredList, ScoreCoveredDetail
+from .views import (TagList, TagDetail, SongOwnTagList, SongCoveredTagList, ScoreOwnList, 
+    ScoreOwnDetail, ScoreCoveredList, ScoreCoveredDetail, songOwnTagDetail, songCoveredTagDetail)
 
 urlpatterns = [
     path('tag/', TagList.as_view()),
     path('tag/<int:pk>', TagDetail.as_view()),
     path('own/tag', SongOwnTagList.as_view()),
-    path('own/tag/<int:pk>', SongOwnTagDetail.as_view()),
+    path('own/tag/<int:pk>', songOwnTagDetail),
+    # path('own/tag/<int:pk>', SongOwnTagDetail.as_view()),
     path('covered/tag', SongCoveredTagList.as_view()),
-    path('covered/tag/<int:pk>', SongCoveredTagDetail.as_view()),
+    path('covered/tag/<int:pk>', songCoveredTagDetail),
     path('score/own', ScoreOwnList.as_view()),
     path('score/own/<int:pk>', ScoreOwnDetail.as_view()),
     path('score/covered', ScoreCoveredList.as_view()),
-    path('score/covered/<int:pk>', ScoreCoveredDetail.as_view())
+    path('score/covered/<int:pk>', ScoreCoveredDetail.as_view()),
 ]
 
 # SongOwnTagDetail, songCoveredTagDetail have to be modified.
--- pt1/evaluation/views.py
+++ pt1/evaluation/views.py
@@ -1,10 +1,14 @@
 from .models import Tag, SongOwnTag, SongCoveredTag, ScoreOwn, ScoreCovered
 from .serializers import TagSerializer, SongOwnTagSerializer, SongCoveredTagSerializer, ScoreOwnSerializer, ScoreCoveredSerializer
 from rest_framework import generics
-from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.decorators import api_view
+from django.http import HttpResponse
+from rest_framework.response import Response
+# from django.http import Http404
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
+# from rest_framework import status
 
 class TagList(generics.ListCreateAPIView):
     """
@@ -36,15 +40,37 @@ class SongOwnTagList(generics.ListCreateAPIView):
     serializer_class = SongOwnTagSerializer
  
 
-# have to be modified
-class SongOwnTagDetail(generics.RetrieveUpdateDestroyAPIView):
-    """
-    date: 2019 - 12 - 13
-    madeby: haein
-    des: GET, PUT, DELETE - 자작곡 - 태그
-    """
-    queryset = SongOwnTag.objects.all()
-    serializer_class = SongOwnTagSerializer
+@api_view(['GET', 'DELETE'])
+def songOwnTagDetail(request, pk):
+    try:
+        song_own_tag_list = SongOwnTag.objects.filter(song=pk)
+    except SongOwnTag.DoesNotExist:
+        return httpResponse(status=404)
+    
+    #show SongOwnTags including SongOwns by pk
+    if request.method == 'GET':
+        serializer = SongOwnTagSerializer(song_own_tag_list, many=True)
+        return Response(serializer.data)
+    
+    #delete SongOwnTags
+    if request.method == 'DELETE':
+        try:
+            song_own_tag = SongOwnTag.objects.get(id=pk)
+        except SongOwnTag.DoewNotExist:
+            return httpResponse(status=404)
+        song_own_tag.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# # have to be modified
+# class SongOwnTagDetail(generics.RetrieveUpdateDestroyAPIView):
+#     """
+#     date: 2019 - 12 - 13
+#     madeby: haein
+#     des: GET, PUT, DELETE - 자작곡 - 태그
+#     """
+#     queryset = SongOwnTag.objects.all()
+#     serializer_class = SongOwnTagSerializer
 
 
 class SongCoveredTagList(generics.ListCreateAPIView):
@@ -57,15 +83,37 @@ class SongCoveredTagList(generics.ListCreateAPIView):
     serializer_class = SongCoveredTagSerializer
 
 
+@api_view(['GET', 'DELETE'])
+def songCoveredTagDetail(request, pk):
+    try:
+        song_covered_tag_list = SongCoveredTag.objects.filter(song=pk)
+    except SongCoveredTag.DoesNotExist:
+        return httpResponse(status=404)
+    
+    #show SongOwnTags including SongOwns by pk
+    if request.method == 'GET':
+        serializer = SongCoveredTagSerializer(song_covered_tag_list, many=True)
+        return Response(serializer.data)
+    
+    #delete SongOwnTags
+    if request.method == 'DELETE':
+        try:
+            song_covered_tag = SongCoveredTag.objects.get(id=pk)
+        except SongCoveredTag.DoewNotExist:
+            return httpResponse(status=404)
+        song_covered_tag.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
 # have to be modified
-class SongCoveredTagDetail(generics.RetrieveUpdateDestroyAPIView):
-    """
-    date: 2019 - 12 - 13
-    madeby: haein
-    des: GET, PUT, DELETE - 자작곡 - 태그
-    """
-    queryset = SongCoveredTag.objects.all()
-    serializer_class = SongCoveredTagSerializer
+# class SongCoveredTagDetail(generics.RetrieveUpdateDestroyAPIView):
+#     """
+#     date: 2019 - 12 - 13
+#     madeby: haein
+#     des: GET, PUT, DELETE - 자작곡 - 태그
+#     """
+#     queryset = SongCoveredTag.objects.all()
+#     serializer_class = SongCoveredTagSerializer
     
 
 class ScoreOwnList(generics.ListCreateAPIView):
