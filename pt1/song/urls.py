7deb60784b12e0257067993882861eaf717dca12
haeinjo <godls036@gmail.com>
2020-01-15 18:07:11 +0900

    add GET API - SongOwn included album

--- pt1/song/urls.py
+++ pt1/song/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import SongOwnList, SongOwnDetail, SongCoveredList, SongCoveredDetail
+from .views import SongOwnList, SongOwnDetail, SongCoveredList, SongCoveredDetail, songOwnPerAlbum
 
 urlpatterns = [
     path('own/', SongOwnList.as_view()),
     path('own/<int:pk>/', SongOwnDetail.as_view()),
     path('covered/', SongCoveredList.as_view()),
-    path('covered/<int:pk>', SongCoveredDetail.as_view()),
+    path('covered/<int:pk>/', SongCoveredDetail.as_view()),
+    path('own/album/<int:pk>/', songOwnPerAlbum),
 ]
--- pt1/song/views.py
+++ pt1/song/views.py
@@ -1,6 +1,10 @@
 from song.models import SongOwn, SongCovered
 from song.serializers import SongOwnSerializer, SongCoveredSerializer
+from album.models import AlbumOwn, AlbumCovered
 from rest_framework import generics
+from rest_framework.decorators import api_view
+from django.http import HttpResponse
+from rest_framework.response import Response
 
 
 class SongList(generics.ListCreateAPIView):
@@ -21,6 +25,24 @@ class SongOwnList(generics.ListCreateAPIView):
     """
     queryset = SongOwn.objects.all()
     serializer_class = SongOwnSerializer
+
+
+@api_view(['GET'])
+def songOwnPerAlbum(request, pk):
+    """
+    date: 2020 - 01 - 15
+    madeby: haein
+    des: GET - 앨번에 수록된 자작곡
+    """
+    try:
+        song_own_list = SongOwn.objects.filter(album=pk)
+    except AlbumOwn.DoesNotExist:
+        return httpResponse(status=404)
+    
+    #show songOwns of album which is pk
+    if request.method == 'GET':
+        serializer = SongOwnSerializer(song_own_list, many=True)
+        return Response(serializer.data)
 
 
 class SongOwnDetail(generics.RetrieveUpdateDestroyAPIView):
